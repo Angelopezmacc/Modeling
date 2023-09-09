c08c9fb2d0d664028ca33d2027f35d94c60757e4
angelopez_macc <angel.lopez@urosario.edu.co>
2023-09-09 15:30:04 -0500

    Cleaning code
    Se añadieron comentarios para aumentar la legibilidad del código. De igual forma, se removieron secciones del mismo que no era útiles

--- main.py
+++ main.py
@@ -1,41 +1,73 @@
-from utils import calc_eigenvalues, calc_eigenvectors, plot_phase_space, system_dynamics
+"""
+
+* Universidad del Rosario
+* Modeling of dynamical systems
+! Lab 4
+! Ángel López
+
+"""
+
+# * Se importan librerías
+from utils import calc_eigenvalues, calc_eigenvectors, plot_phase_space, plot_phase_space_trajectory, system_dynamics, trajectory
 from scipy.integrate import odeint
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Define your system's parameters
-gamma = 0.2
-eta = 0.1
-delta = 0.4
-tau = 0.3
+# * Definición de los parámetros del sistema
+gamma = 1
+eta = 3
+delta = 1
+tau = -1
 
-# Define your system's matrix
+# * Se crea la matriz del sistema y se calculan los eigenvalores y eigenvectores
 A = np.array([[-eta, gamma], [tau, delta]])
-# Calculate eigenvalues and eigenvectors
 eigenvalues = calc_eigenvalues(A)
 eigenvectors = calc_eigenvectors(A)
-
-# Print eigenvalues and eigenvectors
 print("Eigenvalues:", eigenvalues)
 print("Eigenvectors:", eigenvectors)
 
-# Define initial conditions for C and P
+# * Se definen condiciones iniciales
 C0 = 1.0
 P0 = 1.0
-
-# Define time points for which to solve for C and P
 t = np.linspace(0,10)
 
-# Solve system of differential equations to obtain values for C and P at each time point in t
+# * Se resuleve el sistema
 sol = odeint(system_dynamics,[C0,P0],t,args=(gamma,eta,delta,tau))
 
-# Define the grid for the phase space
-c = np.linspace(-2, 2, 20)
-p = np.linspace(-2, 2, 20)
+# * Definición dimensiones gráfica
+c = np.linspace(-2, 2, 200)
+p = np.linspace(-2, 2, 200)
 C, P = np.meshgrid(c, p)
-
-# Calculate the vector field
 U = gamma*P - eta*C
 V = delta*P + tau*C
 
-# Plot phase space
+# * Plot diagrama fase
 plot_phase_space(C, P, U, V, title='Phase Space')
+
+# * Plot diagrama de fase con trayectoria
+n_steps = 100
+cList, pList = trajectory(C0, P0, gamma, eta, delta, tau, n_steps)
+plot_phase_space_trajectory(C, P, U, V, cList=cList, pList=pList, title='Phase Space with Trajectory')
+
+
+
+# * Simulando la trayectoria
+c_trajectory, p_trajectory = trajectory(C0, P0, gamma, eta, delta, tau, n_steps)
+
+# * Plot de la trayectoria
+plt.figure(figsize=(10, 6))
+plt.plot(range(n_steps), c_trajectory, label='Coalition Strength (C)')
+plt.plot(range(n_steps), p_trajectory, label="Parties' Internal Factors (P)")
+plt.xlabel('Time Steps')
+plt.ylabel('Values')
+plt.title('Trajectory of Coalition Strength and Parties Internal Factors')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
